b9b993837f866e88a3eb0233a8cea465039e6d9a
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-02-23 14:07:10 +0100

    function sound + style

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ function makeSound(ev) {
   const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
   console.log(key);
   if (!audio) return; //stop the function from running if the key doesn't belong to our keys
-  audio.currenTime = 0; //rewind the time of the audio
+  audio.currentTime = 0; //rewind the time of the audio
   audio.play();
   key.classList.toggle('playing');
 }
 
 window.addEventListener('keydown', makeSound);
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
