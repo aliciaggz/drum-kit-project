a2bc950087ab0bcc3cb072c497656ed605f7d499
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-02-23 11:53:20 +0100

    some style

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function makeSound(ev) {
   const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
-  console.log(audio);
-
+  const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
+  console.log(key);
   if (!audio) return; //stop the function from running if the key doesn't belong to our keys
+  audio.currenTime = 0; //rewind the time of the audio
   audio.play();
+  key.classList.toggle('playing');
 }
 
 window.addEventListener('keydown', makeSound);
